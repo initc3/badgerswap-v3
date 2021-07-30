145de84944d5e9e053cebb6a84c1a2da9ed00c9e
Sylvain Bellemare <sbellem@gmail.com>
2021-07-30 16:28:54 -0400

    dev

--- ratel/src/python/badgerswapv3/addLiquidity.py
+++ ratel/src/python/badgerswapv3/addLiquidity.py
@@ -4,31 +4,56 @@ from web3 import Web3
 from web3.middleware import geth_poa_middleware
 
 from ratel.src.python.Client import get_inputmasks
-from ratel.src.python.deploy import url, parse_contract, appAddress, tokenAddress, ETH, reserveInput, getAccount
+from ratel.src.python.deploy import (
+    url,
+    parse_contract,
+    appAddress,
+    tokenAddress,
+    ETH,
+    reserveInput,
+    getAccount,
+)
 from ratel.src.python.utils import fp
+
 
 def addLiquidity(appContract, token0, token1, priceLower, priceUpper, maxAmtA, maxAmtB):
     priceLower = int(priceLower * fp)
     priceUpper = int(priceUpper * fp)
     maxAmtA = int(maxAmtA * fp)
     maxAmtB = int(maxAmtB * fp)
-    idxPriceLower, idxPriceUpper, idxMaxAmtA, idxMaxAmtB = reserveInput(web3, appContract, 4, account)
-    maskPriceLower, maskPriceUpper, maskMaxAmtA, maskMaxAmtB = asyncio.run(get_inputmasks(f'{idxPriceLower},{idxPriceUpper},{idxMaxAmtA},{idxMaxAmtB}'))
+    idxPriceLower, idxPriceUpper, idxMaxAmtA, idxMaxAmtB = reserveInput(
+        web3, appContract, 4, account
+    )
+    maskPriceLower, maskPriceUpper, maskMaxAmtA, maskMaxAmtB = asyncio.run(
+        get_inputmasks(f"{idxPriceLower},{idxPriceUpper},{idxMaxAmtA},{idxMaxAmtB}")
+    )
     maskedPriceLower = priceLower + maskPriceLower
     maskedPriceUpper = priceUpper + maskPriceUpper
     maskedMaxAmtA = maxAmtA + maskMaxAmtA
     maskedMaxAmtB = maxAmtB + maskMaxAmtB
-    tx_hash = appContract.functions.addLiquidity(token0, token1, idxPriceLower, maskedPriceLower, idxPriceUpper, maskedPriceUpper, idxMaxAmtA, maskedMaxAmtA, idxMaxAmtB, maskedMaxAmtB).transact()
+    tx_hash = appContract.functions.addLiquidity(
+        token0,
+        token1,
+        idxPriceLower,
+        maskedPriceLower,
+        idxPriceUpper,
+        maskedPriceUpper,
+        idxMaxAmtA,
+        maskedMaxAmtA,
+        idxMaxAmtB,
+        maskedMaxAmtB,
+    ).transact()
     web3.eth.wait_for_transaction_receipt(tx_hash)
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     web3 = Web3(Web3.WebsocketProvider(url))
 
     web3.eth.defaultAccount = web3.eth.accounts[0]
     web3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
-    abi, bytecode = parse_contract('badgerswapv3')
+    abi, bytecode = parse_contract("BadgerSwapV3")
     appContract = web3.eth.contract(address=appAddress, abi=abi)
 
-    account = getAccount(web3, f'/opt/poa/keystore/server_0/')
+    account = getAccount(web3, f"/opt/poa/keystore/server_0/")
     addLiquidity(appContract, ETH, tokenAddress, 1, 2, 10, 10)
--- ratel/src/python/badgerswapv3/run.py
+++ ratel/src/python/badgerswapv3/run.py
@@ -3,33 +3,45 @@ import sys
 
 from web3 import Web3
 
-from ratel.genfiles.python.badgerswapv3 import monitorSecretDeposit, monitorInitPool, monitorAddLiquidity, monitorTrade
+from ratel.genfiles.python.badgerswapv3 import (
+    monitorSecretDeposit,
+    monitorInitPool,
+    monitorAddLiquidity,
+    monitorTrade,
+)
 from ratel.src.python.Server import Server
-from ratel.src.python.deploy import parse_contract, getAccount, preprocessing, appAddress, confirmation, url
+from ratel.src.python.deploy import (
+    parse_contract,
+    getAccount,
+    preprocessing,
+    appAddress,
+    confirmation,
+    url,
+)
 from ratel.src.python.utils import openDB, location_db, http_port, http_host
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     serverID = sys.argv[1]
 
     web3 = Web3(Web3.WebsocketProvider(url))
 
-    account = getAccount(web3, f'/opt/poa/keystore/server_{serverID}/')
+    account = getAccount(web3, f"/opt/poa/keystore/server_{serverID}/")
 
     db = openDB(location_db(serverID))
 
-    abi, bytecode = parse_contract('badgerswapv3')
+    abi, bytecode = parse_contract("BadgerSwapV3")
     appContract = web3.eth.contract(address=appAddress, abi=abi)
 
-    server = Server(
-        serverID, db, http_host, http_port + int(serverID)
-    )
+    server = Server(serverID, db, http_host, http_port + int(serverID))
 
     loop = asyncio.get_event_loop()
-    tasks = [monitorSecretDeposit(web3, db, serverID, appContract, confirmation, account),
-             monitorInitPool(web3, db, serverID, appContract, confirmation, account),
-             monitorAddLiquidity(web3, db, serverID, appContract, confirmation, account),
-             monitorTrade(web3, db, serverID, appContract, confirmation, account),
-             preprocessing(db, appContract, serverID),
-             server.http_server()]
+    tasks = [
+        monitorSecretDeposit(web3, db, serverID, appContract, confirmation, account),
+        monitorInitPool(web3, db, serverID, appContract, confirmation, account),
+        monitorAddLiquidity(web3, db, serverID, appContract, confirmation, account),
+        monitorTrade(web3, db, serverID, appContract, confirmation, account),
+        preprocessing(db, appContract, serverID),
+        server.http_server(),
+    ]
     loop.run_until_complete(asyncio.wait(tasks))
     loop.close()
